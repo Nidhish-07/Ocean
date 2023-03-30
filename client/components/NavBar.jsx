9d08727a2ea9bfa1de8772d8dcfe65d8831f003c
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-03-30 09:53:19 +0530

    Header Added

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -22,6 +22,31 @@ const NavBar = () => {
         <div className="text-[#8a939b] mx-3 font-bold text-lg">
           <AiOutlineSearch />
         </div>
+        <input
+          type="text"
+          placeholder="Search items..."
+          className="h-10 w-full border-0 bg-transparent outline-none ring-0 px-2 pl-0 text-[#e6e8eb] placeholder:text-[#8a939b]"
+        />
+      </div>
+      <div className="flex items-center justify-end">
+        <Link href={"/collections/"}>
+          <div className="px-4 font-bold text-[#c8cacd] hover:text-white cursor-pointer"></div>
+        </Link>
+        <div className="px-4 font-bold text-[#c8cacd] hover:text-white cursor-pointer">
+          Stats
+        </div>
+        <div className="px-4 font-bold text-[#c8cacd] hover:text-white cursor-pointer">
+          Resources
+        </div>
+        <div className="px-4 font-bold text-[#c8cacd] hover:text-white cursor-pointer">
+          Create
+        </div>
+        <div className="px-4 font-bold text-[#c8cacd] hover:text-white cursor-pointer">
+          <CgProfile />
+        </div>
+        <div className="text-[#8a939b] text-3xl font-black px-4 hover:text-white cursor-pointer">
+          <MdOutlineAccountBalanceWallet />
+        </div>
       </div>
     </div>
   );
